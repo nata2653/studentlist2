20f4d303072c4968293cdb0114c980b7e6f48057
Natalie Sirich <nata2653@stud.kea.dk>
2019-09-04 15:22:46 +0200

    add modal and sort

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function start() {
     option.addEventListener("change", filterBy);
   });
 
+  //Filter-loop forEach - addEventlistener for sort
+  document.querySelectorAll("#sort-by").forEach(option => {
+    option.addEventListener("change", sortBy);
+  });
+
   // fetch JSON-data
   async function getJson() {
     let jsonData = await fetch("http://petlatkea.dk/2019/students1991.json");
@@ -69,10 +74,99 @@ function start() {
         //show house in h3
         klon.querySelector(".student h3").innerHTML = student.house;
 
+        klon.querySelector(".student").setAttribute("house", student.house);
+
         dest.appendChild(klon);
       }
     });
+    //Call modal when click
+    document.querySelectorAll(".student").forEach(student => {
+      student.addEventListener("click", showModal);
+    });
+    //Function to show modal
+    function showModal() {
+      let house = this.getAttribute("house");
+      console.log(house);
+
+      console.log("show modal");
+      //If statements to make the modal show depending on choice/click
+      if (house == "Gryffindor") {
+        document.querySelector(".gryffindor").classList.remove("hide");
+        document
+          .querySelector(".gryffindor")
+          .addEventListener("click", function() {
+            document.querySelector(".gryffindor").classList.add("hide");
+          });
+      } else if (house == "Slytherin") {
+        document.querySelector(".slytherin").classList.remove("hide");
+        document
+          .querySelector(".slytherin")
+          .addEventListener("click", function() {
+            document.querySelector(".slytherin").classList.add("hide");
+          });
+      } else if (house == "Ravenclaw") {
+        document.querySelector(".ravenclaw").classList.remove("hide");
+        document
+          .querySelector(".ravenclaw")
+          .addEventListener("click", function() {
+            document.querySelector(".ravenclaw").classList.add("hide");
+          });
+      } else if (house == "Hufflepuff") {
+        document.querySelector(".hufflepuff").classList.remove("hide");
+        document
+          .querySelector(".hufflepuff")
+          .addEventListener("click", function() {
+            document.querySelector(".hufflepuff").classList.add("hide");
+          });
+      }
+    }
   }
+
+  //Function to sort
+  function sortBy() {
+    console.log("Sort json");
+
+    //Change filter by variable
+    sort = this.value;
+
+    console.log(sort);
+
+    // If statement to sort by selection
+    // If firstname sort by firstname
+    if (sort == "Firstname") {
+      console.log(sort);
+
+      //Function to sort by firstname
+      students.sort(function(a, b) {
+        return a.firstname.localeCompare(b.firstname);
+      });
+
+      //If lastname: sort by lastname
+    } else if (sort == "Lastname") {
+      console.log(sort);
+
+      //Function to sort by lastname
+      students.sort(function(a, b) {
+        return a.lastname.localeCompare(b.lastname);
+      });
+
+      //If house sort by house
+    } else if (sort == "House") {
+      console.log(sort);
+
+      //Function to sort by house
+      students.sort(function(a, b) {
+        return a.house.localeCompare(b.house);
+      });
+
+      //Reset sorting
+    } else if (sort == "none") {
+      start();
+    }
+    //Call showStudents to show studenlist again
+    showStudents();
+  }
+  //Function to filter
   function filterBy() {
     console.log("Filter json");
 
@@ -82,7 +176,4 @@ function start() {
     //call function to show studentlist again
     showStudents();
   }
-
-  // Call function to show studentlist again and create loop
-  showStudents();
 }
